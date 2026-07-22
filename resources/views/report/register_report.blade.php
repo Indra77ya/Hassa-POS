f6fa303a57d1630e230e9a2204b3ce1268d57de7
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-07-22 05:25:57 +0000

    Tidy up Sales Order tables, reports, settings, lists and dynamic custom fields visibility

--- resources/views/report/register_report.blade.php
+++ resources/views/report/register_report.blade.php
@@ -39,30 +39,31 @@
     <div class="row">
         <div class="col-md-12">
             @component('components.widget', ['class' => 'box-primary'])
-                <table class="table table-bordered table-striped" id="register_report_table">
-                    <thead>
-                        <tr>
-                            <th>@lang('report.open_time')</th>
-                            <th>@lang('report.close_time')</th>
-                            <th>@lang('sale.location')</th>
-                            <th>@lang('report.user')</th>
-                            <th>@lang('cash_register.total_card_slips')</th>
-                            <th>@lang('cash_register.total_cheques')</th>
-                            <th>@lang('cash_register.total_cash')</th>
-                            <th>@lang('lang_v1.total_bank_transfer')</th>
-                            <th>@lang('lang_v1.total_advance_payment')</th>
-                            <th>{{$payment_types['custom_pay_1']}}</th>
-                            <th>{{$payment_types['custom_pay_2']}}</th>
-                            <th>{{$payment_types['custom_pay_3']}}</th>
-                            <th>{{$payment_types['custom_pay_4']}}</th>
-                            <th>{{$payment_types['custom_pay_5']}}</th>
-                            <th>{{$payment_types['custom_pay_6']}}</th>
-                            <th>{{$payment_types['custom_pay_7']}}</th>
-                            <th>@lang('cash_register.other_payments')</th>
-                            <th>@lang('sale.total')</th>
-                            <th class="not-export">@lang('messages.action')</th>
-                        </tr>
-                    </thead>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped" id="register_report_table" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th style="white-space: nowrap !important;">@lang('report.open_time')</th>
+                                <th style="white-space: nowrap !important;">@lang('report.close_time')</th>
+                                <th style="white-space: nowrap !important;">@lang('sale.location')</th>
+                                <th style="white-space: nowrap !important;">@lang('report.user')</th>
+                                <th style="white-space: nowrap !important;">@lang('cash_register.total_card_slips')</th>
+                                <th style="white-space: nowrap !important;">@lang('cash_register.total_cheques')</th>
+                                <th style="white-space: nowrap !important;">@lang('cash_register.total_cash')</th>
+                                <th style="white-space: nowrap !important;">@lang('lang_v1.total_bank_transfer')</th>
+                                <th style="white-space: nowrap !important;">@lang('lang_v1.total_advance_payment')</th>
+                                <th style="white-space: nowrap !important;">{{$payment_types['custom_pay_1']}}</th>
+                                <th style="white-space: nowrap !important;">{{$payment_types['custom_pay_2']}}</th>
+                                <th style="white-space: nowrap !important;">{{$payment_types['custom_pay_3']}}</th>
+                                <th style="white-space: nowrap !important;">{{$payment_types['custom_pay_4']}}</th>
+                                <th style="white-space: nowrap !important;">{{$payment_types['custom_pay_5']}}</th>
+                                <th style="white-space: nowrap !important;">{{$payment_types['custom_pay_6']}}</th>
+                                <th style="white-space: nowrap !important;">{{$payment_types['custom_pay_7']}}</th>
+                                <th style="white-space: nowrap !important;">@lang('cash_register.other_payments')</th>
+                                <th style="white-space: nowrap !important;">@lang('sale.total')</th>
+                                <th class="not-export" style="white-space: nowrap !important;">@lang('messages.action')</th>
+                            </tr>
+                        </thead>
                     <tfoot>
                         <tr class="bg-gray font-17 text-center footer-total">
                             <td colspan="4"><strong>@lang('sale.total'):</strong></td>
@@ -84,6 +85,7 @@
                         </tr>
                     </tfoot>
                 </table>
+                </div>
             @endcomponent
         </div>
     </div>
@@ -96,5 +98,19 @@
 @endsection
 
 @section('javascript')
+    @php
+        $custom_labels = json_decode(session('business.custom_labels'), true);
+    @endphp
+    <script type="text/javascript">
+        window.register_payment_labels = {
+            custom_pay_1: "{{ !empty($custom_labels['payments']['custom_pay_1']) ? 1 : 0 }}",
+            custom_pay_2: "{{ !empty($custom_labels['payments']['custom_pay_2']) ? 1 : 0 }}",
+            custom_pay_3: "{{ !empty($custom_labels['payments']['custom_pay_3']) ? 1 : 0 }}",
+            custom_pay_4: "{{ !empty($custom_labels['payments']['custom_pay_4']) ? 1 : 0 }}",
+            custom_pay_5: "{{ !empty($custom_labels['payments']['custom_pay_5']) ? 1 : 0 }}",
+            custom_pay_6: "{{ !empty($custom_labels['payments']['custom_pay_6']) ? 1 : 0 }}",
+            custom_pay_7: "{{ !empty($custom_labels['payments']['custom_pay_7']) ? 1 : 0 }}"
+        };
+    </script>
     <script src="{{ asset('js/report.js?v=' . $asset_v) }}"></script>
 @endsection
