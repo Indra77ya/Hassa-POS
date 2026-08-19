54be8765c74916b66388a0ccdb1316ee9da17ac5
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-08-19 16:17:13 +0000

    Add InstallController and installation web routes for AssetManagement module
    - Added `InstallController` in `Modules/AssetManagement/Http/Controllers/InstallController.php` with `index`, `install`, `uninstall`, and `update` methods.
    - Registered `/assetmanagement/install*` web routes in `Modules/AssetManagement/Routes/web.php`.
    - Set `module_version` in `Modules/AssetManagement/Config/config.php`.
    - Verified that Manage Modules UI resolves the Install link (`action('\Modules\AssetManagement\Http\Controllers\InstallController@index')`) and allows superadmins to install the module.

--- Modules/AssetManagement/Config/config.php
+++ Modules/AssetManagement/Config/config.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'name' => 'AssetManagement'
+    'name' => 'AssetManagement',
+    'module_version' => '1.0',
 ];
--- Modules/AssetManagement/Routes/web.php
+++ Modules/AssetManagement/Routes/web.php
@@ -4,10 +4,19 @@ use Illuminate\Support\Facades\Route;
 use Modules\AssetManagement\Http\Controllers\AssetController;
 use Modules\AssetManagement\Http\Controllers\AssetCategoryController;
 use Modules\AssetManagement\Http\Controllers\AssetSettingController;
+use Modules\AssetManagement\Http\Controllers\InstallController;
 
 Route::middleware(['web', 'SetSessionData', 'auth', 'language', 'timezone', 'AdminSidebarMenu'])->group(function () {
     Route::resource('assets', AssetController::class);
     Route::resource('asset-categories', AssetCategoryController::class)->except(['create', 'show', 'edit']);
     Route::get('asset-settings', [AssetSettingController::class, 'index'])->name('asset-settings.index');
     Route::post('asset-settings', [AssetSettingController::class, 'store'])->name('asset-settings.store');
+});
+
+// Installation routes
+Route::middleware(['web', 'SetSessionData', 'auth', 'language', 'timezone'])->prefix('assetmanagement')->group(function () {
+    Route::get('install', [InstallController::class, 'index']);
+    Route::post('install', [InstallController::class, 'install']);
+    Route::get('install/uninstall', [InstallController::class, 'uninstall']);
+    Route::get('install/update', [InstallController::class, 'update']);
 });
