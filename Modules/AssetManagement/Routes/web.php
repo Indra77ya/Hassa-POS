3789faaaa94b5d1b7040398e8ea996c405ce8387
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-08-19 14:25:45 +0000

    Fix InvalidArgumentException for AssetManagement routes by using FQCN references

--- Modules/AssetManagement/Routes/web.php
+++ Modules/AssetManagement/Routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\AssetManagement\Http\Controllers\AssetCategoryController;
+use Modules\AssetManagement\Http\Controllers\AssetController;
+use Modules\AssetManagement\Http\Controllers\AssetDepreciationController;
 
 Route::middleware(['web', 'auth', 'SetSessionData', 'language', 'timezone', 'AdminSidebarMenu'])->prefix('asset-management')->group(function () {
     // Categories
-    Route::resource('categories', 'AssetCategoryController')->except(['show']);
+    Route::resource('categories', AssetCategoryController::class)->except(['show']);
 
     // Assets
-    Route::resource('assets', 'AssetController')->except(['show']);
+    Route::resource('assets', AssetController::class)->except(['show']);
 
     // Depreciation
-    Route::get('depreciations', 'AssetDepreciationController@index')->name('assetmanagement.depreciation.index');
-    Route::post('depreciations/run', 'AssetDepreciationController@runDepreciationOnDemand')->name('assetmanagement.depreciation.run');
+    Route::get('depreciations', [AssetDepreciationController::class, 'index'])->name('assetmanagement.depreciation.index');
+    Route::post('depreciations/run', [AssetDepreciationController::class, 'runDepreciationOnDemand'])->name('assetmanagement.depreciation.run');
 });
