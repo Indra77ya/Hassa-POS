228c7fdba49d7c88e4ef557b15c87bed02517bc3
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-09-03 10:35:16 +0000

    Ensure consistent AdminLTE button styling across POS header and module partials

--- Modules/Repair/Resources/views/layouts/partials/pos_header.blade.php
+++ Modules/Repair/Resources/views/layouts/partials/pos_header.blade.php
@@ -1,10 +1,7 @@
 @if($__is_repair_enabled)
 	@can("repair.create")
-		<a 
-		class="tw-shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] tw-bg-white hover:tw-bg-white/60 tw-cursor-pointer tw-border-2 tw-w-auto tw-h-auto tw-py-1 tw-px-4 tw-rounded-md"
-		href="{{ action([\App\Http\Controllers\SellPosController::class, 'create']). '?sub_type=repair'}}" title="{{ __('repair::lang.add_repair') }}" data-toggle="tooltip" data-placement="bottom" class="tw-dw-btn tw-dw-btn-xs tw-dw-btn-outline tw-dw-btn-primary m-6 btn-xs m-5 pull-right">
-			<i class="fa fa-wrench fa-lg !tw-text-sm"></i>
-			<strong>@lang('repair::lang.repair')</strong>
+		<a class="btn btn-info btn-flat btn-sm" href="{{ action([\App\Http\Controllers\SellPosController::class, 'create']). '?sub_type=repair'}}" title="{{ __('repair::lang.add_repair') }}" data-toggle="tooltip" data-placement="bottom" style="font-weight: bold; height: 30px; display: inline-flex; align-items: center;">
+			<i class="fa fa-wrench"></i> &nbsp;@lang('repair::lang.repair')
 		</a>
 	@endcan
 @endif
