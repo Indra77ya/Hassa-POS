f39a88655f19c3e687bbef96ab13c2ff4e998af1
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-09-03 07:28:27 +0000

    Update Superadmin dashboard and navigation to AdminLTE UI style

--- Modules/Superadmin/Resources/views/layouts/nav.blade.php
+++ Modules/Superadmin/Resources/views/layouts/nav.blade.php
@@ -1,9 +1,9 @@
 <section class="no-print">
-    <nav class="navbar-default tw-transition-all tw-duration-5000 tw-shrink-0 tw-rounded-2xl tw-m-[16px] tw-border-2 !tw-bg-white">
+    <nav class="navbar navbar-default">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" style="margin-top: 3px; margin-right: 3px;">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
