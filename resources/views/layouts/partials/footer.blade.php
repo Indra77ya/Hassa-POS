063570235fe8c333b424d65bf45900fde4d0a3a6
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-09-10 02:31:03 +0000

    Fix footer copyright text overlap in application layout

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 
 
-<div class="tw-mt-auto clearfix" style="clear: both;">
-  <div class="tw-mb-4 tw-ms-8 -tw-mt-1 no-print">
+<div class="tw-mt-auto tw-pt-8 tw-pb-6 clearfix" style="clear: both;">
+  <div class="tw-ms-8 no-print">
     <p class="tw-text-xs tw-font-normal tw-text-gray-500">
       {{ config('app.name', 'ultimatePOS') }} - <span class="tw-font-mono tw-font-medium"> V{{config('author.app_version')}}</span> | Copyright &copy; {{ date('Y') }} All rights reserved.
     </p>
